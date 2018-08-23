46a0e826a74d2367cbdc3c1c27812e00e434db4c
scheung38 <sebastian_cheung@yahoo.com>
2018-08-23 16:17:19 +0800

    initial commit

---
+++ src/main/kotlin/com/example/maven/kotlin/Hello.kt
@@ -0,0 +1,8 @@
+package com.example.maven.kotlin
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+
+
+}
+
---
+++ src/test/kotlin/com/example/maven/kotlin/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.example.maven.kotlin
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
