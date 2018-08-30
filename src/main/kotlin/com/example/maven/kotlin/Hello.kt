83b71e33bb74d6e0c95fd83fb2fe497e32b96b98
scheung38 <sebastian_cheung@yahoo.com>
2018-08-30 10:18:21 +0800

    initial commit

--- src/main/kotlin/com/example/maven/kotlin/Hello.kt
+++ src/main/kotlin/com/example/maven/kotlin/Hello.kt
@@ -1,8 +1,48 @@
 package com.example.maven.kotlin
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import java.lang.Exception
+import java.io.RandomAccessFile
+
 fun main(args: Array<String>) {
     println("Hello, World")
 
+    while (true) println("SPAM")
 
+    // val ary = arrayOf("/lib/CBLCALLER_1534494940.cczip")
+    // val ary = arrayOf("/lib/CBLCALLER_1534494940.cczip")
+
+    //    testXml() // no need to XML parse, jse directly from zip file
+
+//    try {
+//          val results = CCResultsFactory
+//        print(ary.get(0))
+//    } catch (e: Exception){
+
+//    } finally {
+
+//    }
 }
+
+//data class Data1(val id: Int,
+//                 val first: String,
+//                 val last: String)
+
+//fun testXml() {
+//    val xmlString = "<Customer>" +
+//            "  <name>Mary</name>" +
+//            "  <age>37</age>" +
+//            "  <address>" +
+//            "  <street>NANTERRE CT</street>" +
+//            "  <postcode>77471</postcode>" +
+//            "  </address>" +
+//            "</Customer>"
+//
+//    val xmlMapper = XmlMapper()
+//
+//    val obj = xmlMapper.readValue(xmlString, Data1::class.java)
+//
+////    println("obj is $obj")
+//}
+
 
